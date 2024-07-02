b9339d8df1ee8ad1492600cab4c0305b4d78095c
shifu-dev <csingh8476@gmail.com>
2024-07-02 17:33:41 +0530

    update: add doxygen and theme.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    outputs = inputs:
+    let
+        system = "x86_64-linux";
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+        llvmPackages = pkgs.llvmPackages_18;
+        pythonPackages = pkgs.python310Packages;
+        stdenv = llvmPackages.stdenv;
+    in
+    {
+        devShells.${system}.default = stdenv.mkDerivation {
+            name = "atom-doc";
+            src = ./.;
+
+            nativeBuildInputs = with pkgs; [
+                doxygen
+                graphviz
+                cmake
+                ninja
+                git
+            ];
+        };
+    };
+}
